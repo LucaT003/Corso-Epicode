d85c568f6763fa6af2f0d2cefb64bf3108de68a9
Luca Trimboli <106075804+LucaT003@users.noreply.github.com>
2022-07-01 17:01:12 +0200

    Add files via upload

---
+++ Esercizio_week6/script.js
@@ -0,0 +1,12 @@
+var ham = document.getElementById('ham');
+
+function f() {
+    addEventListener (ham, 'click')
+    if (ham.area-epanded (false))  {
+        ham = display-none
+    }else {
+        ham = display
+    } 
+    
+
+}
